a5710eb7b19333c8b43bc735029e9536e2c33525
jimmynotjim <jimmynotjim@me.com>
2013-12-19 16:19:21 -0500

    Initial generator assets

--- app/index.js
+++ app/index.js
@@ -2,49 +2,179 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
+//var chalk = require('chalk');
 
 
-var GenesisThemeGenerator = module.exports = function GenesisThemeGenerator(args, options, config) {
+var WpThemeGenerator = module.exports = function WpThemeGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+  this.prompts = [];
+  this.bower = JSON.parse(this.readFileAsString(path.join(this.env.cwd, 'bower.json')));
+
   this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
+    this.installDependencies({
+      bower: true,
+      npm: true,
+      skipInstall: options['skip-install'],
+      skipMessage: true
+    });
   });
 
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+  this.sourceRoot(path.join(__dirname, 'templates'));
 };
 
-util.inherits(GenesisThemeGenerator, yeoman.generators.Base);
+util.inherits(WpThemeGenerator, yeoman.generators.Base);
 
-GenesisThemeGenerator.prototype.askFor = function askFor() {
-  var cb = this.async();
+WpThemeGenerator.prototype.promptForName = function() {
+  var existing = function() {
+    try {
+      return this.bower.name;
+    } catch(e) {};
+  }.bind(this);
+
+  this.prompts.push({
+    required: true,
+    type: 'text',
+    name: 'projName',
+    message: 'Project Name (e.g. MySite)',
+    default: function() {
+      return existing() || path.basename(this.env.cwd);
+    }.bind(this)
+  });
+};
+
+WpThemeGenerator.prototype.promptForShortName = function() {
+  var existing = function() {
+    try {
+      return this.bower.shortName;
+    } catch(e) {};
+  }.bind(this);
+
+  this.prompts.push({
+    required: true,
+    type: 'text',
+    name: 'projShortName',
+    message: 'Project short name (e.g. MS)',
+    default: function() {
+      return existing() || path.basename(this.env.cwd).replace(/[^A-Z]/g, '');
+    }.bind(this)
+  });
+};
+
+WpThemeGenerator.prototype.promptForAuthorName = function() {
+  var existing = function() {
+    try {
+      return this.bower.author.name;
+    } catch(e) {};
+  }.bind(this);
+
+  this.prompts.push({
+    type: 'text',
+    name: 'authorName',
+    message: 'Author Name (e.g. John Smith)',
+    default: function() {
+      return existing() || '';
+    }.bind(this)
+  });
+};
+
+WpThemeGenerator.prototype.promptForAuthorURI = function() {
+  var existing = function() {
+    try {
+      return this.bower.author.url
+    } catch(e) {};
+  }.bind(this);
+
+  this.prompts.push({
+    type: 'text',
+    name: 'authorURI',
+    message: 'Author URI (e.g. www.johnsmith.com)',
+    default: function() {
+      return existing() || '';
+    }.bind(this)
+  });
+};
+
+WpThemeGenerator.prototype.promptForDescription = function() {
+  var existing = function() {
+    try {
+      return this.bower.description;
+    } catch(e) {};
+  }.bind(this);
+
+  this.prompts.push({
+    type: 'text',
+    name: 'projDescription',
+    message: 'Project Description',
+    default: function() {
+      return existing() || '';
+    }.bind(this)
+  });
+};
+
+WpThemeGenerator.prototype.promptForVersion = function() {
+  var existing = function() {
+    try {
+      return this.bower.version;
+    } catch(e) {};
+  }.bind(this);
+
+  this.prompts.push({
+    type: 'text',
+    name: 'projVersion',
+    message: 'Project version',
+    default: function() {
+      return existing() || '0.1.0';
+    }.bind(this)
+  });
+};
+
+WpThemeGenerator.prototype.promptForWeb = function() {
+  this.prompts.push({
+    type: 'text',
+    name: 'web',
+    message: 'WordPress directory',
+    default: 'web'
+  });
+};
+
+WpThemeGenerator.prototype.ask = function() {
+  var done = this.async();
 
   // have Yeoman greet the user.
   console.log(this.yeoman);
 
-  var prompts = [{
-    type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
-    default: true
-  }];
+  this.prompt(this.prompts, function(props) {
+    this.props = props;
 
-  this.prompt(prompts, function (props) {
-    this.someOption = props.someOption;
-
-    cb();
+    done();
   }.bind(this));
+
 };
 
-GenesisThemeGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/templates');
-
-  this.copy('_package.json', 'package.json');
-  this.copy('_bower.json', 'bower.json');
+WpThemeGenerator.prototype.ready = function() {
+  this.log.write('\n');
+  this.log.info('Here we go!');
 };
 
-GenesisThemeGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy('editorconfig', '.editorconfig');
-  this.copy('jshintrc', '.jshintrc');
+WpThemeGenerator.prototype.writeProjectFiles = function() {
+  this.log.info('Writing project files...');
+
+  this.copy('Gruntfile.js', 'Gruntfile.js');
+
+  this.template('editorconfig', '.editorconfig');
+  this.template('jshintrc', '.jshintrc');
+  this.template('bower.json', 'bower.json');
+  this.template('package.json', 'package.json');
+  //this.template('README.md', 'README.md');
 };
+
+WpThemeGenerator.prototype.writeThemeFiles = function() {
+  this.log.info('Writing theme files...');
+
+  this.directory('themes/Genesis-Parent-theme', path.join(this.props.web, 'wp-content/themes/Genesis-Parent-theme'));
+  this.directory('themes/Genesis-Child-theme', path.join(this.props.web, 'wp-content/themes/', this.props.projShortName + '-theme'));
+};
+
+module.exports = WpThemeGenerator;
---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,204 @@
+'use strict';
+module.exports = function(grunt) {
+
+  // Load Grunt tasks declared in the package.json file
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // Save the package.json data for variable usage
+    pkg:    grunt.file.readJSON('package.json'),
+
+    // Set up the banner from the package.json data
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %> */\n',
+
+    // Constants
+    NAME:       '<%= pkg.name %>',
+    SHORT_NAME: '<%= pkg.shortName %>',
+    BOWER_DIR:  'bower_components',
+    ASSETS_DIR: 'web/wp-content/themes/<%= SHORT_NAME %>-theme/assets',
+    DIST_DIR:   '<%= ASSETS_DIR %>/dist',
+    DEV_DIR:    '<%= ASSETS_DIR %>/dev',
+    SRC_DIR:    '<%= ASSETS_DIR %>/src',
+    IMG_DIR:    '<%= ASSETS_DIR %>/img',
+    IMG_FILES:  '**/*.{png,gif,jpg,jpeg}',
+
+    // Task configuration
+
+    // Cleans the appropriate directory to prepare for compiled source files
+    clean: {
+      dev: {
+        src: '<%= DEV_DIR %>/*'
+      },
+      dist: {
+        src: '<%= DIST_DIR %>/*'
+      }
+    },
+
+    // Concatonates the source JS files to header, footer and single files
+    concat: {
+      options: {
+        banner:       '<%= banner %>',
+        stripBanners: true
+      },
+      header: {
+        src: [
+          '<%= SRC_DIR %>/js/header/libs/**/*.js',
+          '<%= SRC_DIR %>/js/header/**/*.js'
+        ],
+        dest: '<%= DEV_DIR %>/<%= SHORT_NAME.toLowerCase() %>-header.js'
+      },
+      footer: {
+        src: [
+          '<%= SRC_DIR %>/js/footer/libs/**/*.js',
+          '<%= SRC_DIR %>/js/footer/**/*.js'
+        ],
+        dest: '<%= DEV_DIR %>/<%= SHORT_NAME.toLowerCase() %>-footer.js'
+      },
+      single: {
+        src: [
+          '<%= SRC_DIR %>/js/single/libs/**/*.js',
+          '<%= SRC_DIR %>/js/single/**/*.js'
+        ],
+        dest: '<%= DEV_DIR %>/<%= SHORT_NAME.toLowerCase() %>-single.js'
+      }
+    },
+
+    // Minifies the concatonated js files from the /dev dir to the /dist dir
+    uglify: {
+      all: {
+        options: {
+          banner: '<%= banner %>'
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= DEV_DIR %>/',
+          src: '**/*.js',
+          dest: '<%= DIST_DIR %>/',
+          ext: '.min.js'
+        }]
+      }
+    },
+
+    // Lints all the js files for errors
+    jshint: {
+      gruntfile: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: 'Gruntfile.js'
+      },
+      src: {
+        options: {
+          jshintrc: '<%= SRC_DIR %>/.jshintrc'
+        },
+        src: [
+          '<%= SRC_DIR %>/js/**/*.js',
+          '!**/libs/**/*.js'
+        ]
+      }
+    },
+
+    // Compiles the sass from the source dir
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed',
+          force: true
+        },
+        src:  '<%= SRC_DIR %>/scss/style.scss',
+        dest: '<%= DIST_DIR %>/style.min.css'
+      },
+      dev : {
+        options: {
+          style:        'expanded',
+          lineNumbers:  true
+        },
+        src:  '<%= SRC_DIR %>/scss/style.scss',
+        dest: '<%= DEV_DIR %>/style.css'
+      }
+    },
+
+    // Minimizes all the images to the /min directory
+    imagemin: {
+      all: {
+        options: {},
+        files: [{
+          expand: true,
+          cwd: '<%= IMG_DIR %>/src/',
+          src: '<%= IMG_FILES %>',
+          dest: '<%= IMG_DIR %>/min/'
+        }]
+      }
+    },
+
+    // Sets up grunt to watch for file changes and fire the appropriate task
+    watch: {
+      sass: {
+        files: '<%= SRC_DIR %>/scss/**/*.scss',
+        tasks: 'sass:dev'
+      },
+      js: {
+        files: '<%= SRC_DIR %>/js/**/*.js',
+        tasks: [
+          'jshint:src',
+          'concat:header',
+          'concat:footer',
+          'concat:single'
+        ]
+      },
+      img: {
+        files: '<%= IMG_DIR %>/<%= IMG_FILES %>',
+        tasks: [
+          'imagemin:all'
+        ]
+      },
+      livereload: {
+        options: {
+          livereload: true
+        },
+        files: [
+          '<%= DEV_DIR %>/**',
+          '*.php',
+          '**/*.php'
+        ]
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', [
+    'jshint',
+    'concat',
+    'uglify'
+  ]);
+
+  // easier to remember than jshint
+  grunt.registerTask('lint', [
+    'jshint'
+  ]);
+
+  // Build for distribution (staging or production)
+  grunt.registerTask('build', [
+    'clean:dist',
+    'jshint',
+    'concat:header',
+    'concat:footer',
+    'concat:single',
+    'uglify:all',
+    'sass:dist',
+    'imagemin:all'
+  ]);
+
+  // Build for local dev
+  grunt.registerTask('build:dev', [
+    'jshint:src',
+    'concat:header',
+    'concat:footer',
+    'concat:single',
+    'sass:dev',
+    'imagemin:all'
+  ]);
+};
