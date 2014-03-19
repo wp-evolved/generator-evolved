dfb5a69ef1a30c731fc8e8a99868ee0ae6c21c01
jimmynotjim <jimmynotjim@me.com>
2014-03-18 23:04:39 -0400

    Added mock options and included expected files/dirs
    All tests passing, hopefully that means we're doing things right and not that I'm not testing aggressively enough

--- test/test-creation.js
+++ test/test-creation.js
@@ -12,27 +12,44 @@ describe('theme generator', function () {
                 return done(err);
             }
 
-            this.app = helpers.createGenerator('theme:app', [
+            this.app = helpers.createGenerator('genesis-theme:app', [
                 '../../app'
             ]);
             done();
         }.bind(this));
     });
 
+    it('the generator can be required without throwing', function() {
+        this.app = require('../app');
+    });
+
     it('creates expected files', function (done) {
         var expected = [
             // add files you expect to exist here.
+            ['bower.json', /"name": "temp-site"/],
+            ['package.json', /"name": "temp-site"/],
             '.jshintrc',
-            '.editorconfig'
+            '.editorconfig',
+            'Gruntfile.js',
+            'web/wp-content/themes/genesis-parent-theme',
+            'web/wp-content/themes/ts-theme'
         ];
 
         helpers.mockPrompt(this.app, {
-            'someOption': true
+            'projName': 'temp-site',
+            'projShortName': 'ts',
+            'authorName': 'Test Author',
+            'authorURI': 'test-author.com',
+            'projDescription': 'test site just for testing',
+            'projVersion': '0.1.1'
         });
+
         this.app.options['skip-install'] = true;
+
         this.app.run({}, function () {
             helpers.assertFiles(expected);
             done();
         });
     });
+
 });
