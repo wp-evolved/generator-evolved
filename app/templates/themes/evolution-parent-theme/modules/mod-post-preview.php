f7b382f97389dcee5cbd341b0002a359d051b776
germanny <germanny@gmail.com>
2014-06-20 15:14:49 -0500

    remove title attribute from entry-title bookmark

--- app/templates/themes/evolution-parent-theme/modules/mod-post-preview.php
+++ app/templates/themes/evolution-parent-theme/modules/mod-post-preview.php
@@ -3,7 +3,7 @@
   <?php display_post_thumbnail( get_the_ID(), 'post-thumb' ); // name of the thumbnail; default is 'thumbnail' ?>
 
   <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <h3 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php echo get_the_title(); ?>"><?php the_title(); ?></a></h3>
+    <h3 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
     <div class="post-meta">
       <time class="updated published" datetime="<?php the_date( 'c' ); ?>"><?php the_time('F j, Y'); ?></time>
       <span class="author vcard"><span class="fn"><?php the_author(); ?></span></span>
